1737a8389fa628762e5760bbdb41329e9179d67e
mosm <mosm@me.com>
2015-08-19 21:20:26 +0200

    initial version
    runAnalysis.R script and README.md

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # HumanActivityRecognition
 Human Activity Recognition Using Smartphones Data Set - Assignment 1 Getting and Cleaning Data
+
+## Introduction
+The aim of this analysis is to provide a tidy set of data containing average files for mean and std variables from a set of experiments conducted on 6 activities for 30 subjects wearing a Samsung Galaxy S II phone. 
+
+This repository contains:
+1. run_analysis.R - an R script loading the original dataset, selecting relevant information from that data set and tidying it. 
+2. Codebook - A description of the variables in the tidy data set produced by the run_analysis.R script
+3. README.md - A markdown file describing what steps have been taken to produce the tidy data set
+
+## run_analysis.R
+This script can be used to produce the tidy data set. 
+### steps
+The following steps were taken. Each step will be described in more detail
+1. Loading the provided data
+2. Merging training and test data
+3. Labelling the variables
+4. Loading activity descriptions
+5. Selecting all variables that represent mean and std measures
+6. Make variable names more descriptive
+7. Merge activity descriptions with data set
+8. Remove acitivty number due to redundancy
+9. Create tidy data set, aggregating on subject,activity and averaging the numbers
+10. Write tidy data set
+
+#### Loading the data
+The data is assumed to be available in the working directory. The data contains a folder structure, where the folder 'UCT HAR Dataset' is the folder in the working directory containing some description files and 2 subfolders: 'train' and 'test'. These contain 3 files each with measurement (X_train.txt and C_test.txt), activity (y_train.txt and y_test.txt) and subject (subject_train.txt and subject_test.txt) data. 
+
+The measurment, activity and subject files contain the same number of lines and are combined in one data.frame with cbind
+
+#### Merging the data
+The two combined datasets are then combined in one data.frame with rbind.
+
+#### Labelling the variables
+Feature names are retrieved from the file  'UCI HAR Dataset/features.txt'. These are used to name all columns in the combined dataset but the first two, which are 'activity_number' and 'person'. 
+
+#### Selecting variables
+The request is only to retain variables that represent mean() or std() measures. These are retrieved from the column names through searching for the substring mean() and std(). Together with 'activity_number' and 'person' these columns are selected and stored in the data.frame 'selection'
+
+#### Make variable names more descriptive
+The following steps were taken to make the variable names more descriptive:
+1. retrieve the column names in a list cols
+2. replace 'Gyro' with full text 'Gyroscope'
+3. replace 'Acc' with full text 'Acceleration'
+4. Replace '-mean()' with 'Mean'
+5. Replace '-std()' with 'StandardDeviation'
+6. Replace 'Mag' with 'Magnitude'
+7. Replace start of string 't' with 'Time'
+8. Replace start of string 'f' with 'Frequency'
+9. replace '-X', '-Y', '-Z' with 'X-axis', 'Y-axis', 'Z-axis'
+10. reset column names of data.frame selection with updated cols list
+
+#### Merge activity descriptions with data set
+The activity descriptions have been loaded in a data.frame activity_label. This data.frame is merged with the selection
+
+#### Remove activity number
+Selection now contains both activity_number and activity_description. The first one is removed to prevent redundancy
+
+#### Create tidy data set
+Tidy data should contain one variable per column and contain one observation per row. In this set one observation has been interpreted as a person/activity combination. 
+Data per measure is averaged per person/activity combination and stored in a wide data set with 66 variable columns (in addition to person and activity_description)
+Data is averaged with the aggregate command, with a mean parameter. The 66 variables are therefore averaged per person/activity combination
+This leads to a set with 180 rows and 68 columns. The names of the first two columns have been lost with the aggregate command and are reset with the rename command. 
+
+#### write tidy data set
+The tidy data set is then stored in the working directory with the write.table command to the file 'assignment1.txt'. 
+row.names 
---
+++ run_Analysis.R
@@ -0,0 +1,84 @@
+## run_Analysis.R
+# This is the R script for loading and tidying data for course project 1 of the Getting and Cleaning Data MOOC
+# The script contains comments for the statements used. A more general explanation of the approach is available in the Readme markdown
+# file available in the github repo. 
+# There you can also find the code book for the variables selected. 
+
+library(dplyr)
+# Load train measurements
+# Specifications state that the script should work as long as the Samsung data is in the working directory. I have assumed this means
+# the data should be in the working directory in the original folder structure, i.e. the directory UCI HAR Dataset should be in the 
+# working directory. 
+train_set <- read.table("UCI HAR Dataset/train/X_train.txt", fill=TRUE, header=FALSE)
+# Load train activities
+train_activity <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE)
+# Loiad train subjects
+train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
+# Combine train measurements, activities and subjects in 1 data frame
+train_res <- cbind(train_activity, train_subject, train_set)
+# Load test meausurements
+test_set <- read.table("UCI HAR Dataset/test/X_test.txt", fill=TRUE, header=FALSE)
+# Load test activities
+test_activity <- read.table("UCI HAR Dataset/test/y_test.txt", fill=TRUE, header=FALSE)
+# Load test subjects
+test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE)
+# Combine test measurements, activities and subjects in 1 data frame
+test_res <- cbind(test_activity, test_subject, test_set)
+# Combine train and test set in one new data.frame
+res <- rbind(train_res, test_res)
+# Load measurement labels
+label <- read.table("UCI HAR Dataset/features.txt", header=FALSE, stringsAsFactors = FALSE)
+# Set column names, with "activity_number" and "person" as first two columns, then the read labels
+colnames(res) <- c("activity_number", "person", label$V2)
+# Read descriptive activity names
+activity_label=read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE, header=FALSE)
+# Set column names for descriptive activity names. 
+colnames(activity_label) <-  c("activity_number", "activity_description")
+# Retrieve column names
+cols <- colnames[res]
+# Determine which variable names end in std() or mean()
+matches <- grep("mean\\(\\)|std\\(\\)", cols)
+# Store solumn names for matches in varsToRetain
+varsToRetain <- cols[matches]###
+# Select varsToRetain (in addition to mean and std measurements, this includes the activity_number and subject)
+selection <- res[,c("activity_number", "person", varsToRetain)]
+
+# Improve on variable naming. First get current column names
+cols <- colnames[selection]
+# Replace "Gyro" with full text "Gyroscope"
+cols <- gsub("Gyro", "Gyroscope", cols)
+# Replace "Acc" with full text "Acceleration"
+cols <- gsub("Acc", "Acceleration", cols)
+# Replace "-mean()" with noun "Mean" 
+cols <- gsub("-mean\\(\\)", "Mean", cols)
+# Replace "-std()" with noun "Standard Deviation"
+cols <- gsub("-std\\(\\)", "StandardDeviation", cols)
+# Replace "Mag" with "Magnitude"
+cols <- gsub("Mag", "Magnitude", cols)
+# Replace letter t at beginning of string with "Time"
+cols <- sub("^t", "Time", cols)
+# Replace letter f at beginning of string with "Frequency"
+cols <- sub("^f", "Frequency", cols)
+# Replace "-X", "-Y", "-Z" at end of string with "X-Axis", etc. 
+cols <- sub("-X", "X-Axis", cols)
+cols <- sub("-Y", "Y-Axis", cols)
+cols <- sub("-Z", "Z-Axis", cols)
+# Replace colnames of selection with revised variable names
+colnames(selection) <- cols
+
+# merge activity_label and selection. This will be done on the basis of their common variable activity_number, 
+# adding an activity_description column
+
+selection <- merge(activity_label, selection)
+
+## Averaging and tidying the data
+
+# Remove activity_number as this is redundant information
+selection <- selection[,-1]
+
+# Create table with average values per activity/subject combination. 
+# I chose a wide format, with each row describing an activity/person combination, followed by the average for all measurements
+tidyset <- aggregate(selection[,3:68], list(selection$activity_description, selection$person) , mean)
+# Set column names for acitvity_description and person
+tidyset <- rename(tidyset, activity_description=Group.1,person=Group.2)
+write.table(tidyset, file="assignment1.txt",row.names=FALSE)
